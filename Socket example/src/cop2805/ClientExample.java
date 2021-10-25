1814f13a5fb95133ff7495d315311c54b63c012a
Jason Graves <92340845+JasonGraves96@users.noreply.github.com>
2021-10-25 19:05:02 -0400

    Add files via upload

---
+++ Socket example/src/cop2805/ClientExample.java
@@ -0,0 +1,34 @@
+package cop2805;
+import java.net.*;
+import java.io.*;
+import java.nio.charset.*;
+
+public class ClientExample {
+	public static void main(String[] args) {
+		System.out.print("Input a string: ");
+		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+		try
+		{
+			String userString = userInput.readLine();
+			Socket connection = new Socket("127.0.0.1", 1237);
+			InputStream input = connection.getInputStream();
+			OutputStream output = connection.getOutputStream();
+			
+			output.write(userString.length());
+			output.write(userString.getBytes());
+			
+			int n = input.read();
+			byte[] data = new byte[n];
+			input.read(data);
+			
+			String serverResponse = new String(data, StandardCharsets.UTF_8);
+			System.out.println("Server said: " + serverResponse);
+			
+			if(!connection.isClosed())
+				connection.close();
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
